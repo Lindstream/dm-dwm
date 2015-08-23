4af8afe61865203f977fc431958e03226b28c773
Robin Lindström <robin@lindstream.com>
2015-08-23 16:58:00 +0200

    statusallmons added

--- dwm.c
+++ dwm.c
@@ -756,17 +756,13 @@ drawbar(Monitor *m) {
 	drw_text(drw, x, 0, w, bh, m->ltsymbol, 0);
 	x += w;
 	xx = x;
-	if(m == selmon) { /* status is only drawn on selected monitor */
-		w = TEXTW(stext);
-		x = m->ww - w;
-		if(x < xx) {
-			x = xx;
-			w = m->ww - xx;
-		}
-		drw_text(drw, x, 0, w, bh, stext, 0);
+	w = TEXTW(stext);
+	x = m->ww - w;
+	if(x < xx) {
+		x = xx;
+		w = m->ww - xx;
 	}
-	else
-		x = m->ww;
+	drw_text(drw, x, 0, w, bh, stext, 0);
 	if((w = x - xx) > bh) {
 		x = xx;
 		if(m->sel) {
@@ -2002,9 +1998,11 @@ updatetitle(Client *c) {
 
 void
 updatestatus(void) {
+	Monitor* m;
 	if(!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
 		strcpy(stext, "dwm-"VERSION);
-	drawbar(selmon);
+	for(m = mons; m; m = m->next)
+		drawbar(m);
 }
 
 void
