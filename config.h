e39b47324e0cb07b221486101fa752a373c86054
Robin Lindström <robin@lindstream.com>
2015-08-25 10:25:50 +0200

    Added more commands

--- config.h
+++ config.h
@@ -54,15 +54,15 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class               instance    title       tags mask     iscentered  isfloating   monitor */
-  { "CherryTree",        NULL,       NULL,       1 << 3,       True,       True,       -1 },
-  { "Deadbeef",          NULL,       NULL,       1 << 4,       True,       True,       -1 },
-	{ "Deluge",            NULL,       NULL,       1 << 7,       True,       True,       -1 },
-  { "Firefox",           NULL,       NULL,       1 << 1,       False,      False,      -1 },
-  {  "Termite",          NULL,       "WeeChat 1.2",  1 << 6,       False,      False,      -1 },
-  {  NULL,               NULL,       "glances",  1 << 4,       False,      False,      -1 },
-  { "Subl3",             NULL,       NULL,       1 << 2,       False,      False,      -1 },
-	{ "GIMP",              NULL,       NULL,       1 << 4,       False,      False,      -1 },
+	/* class               instance    title              tags mask     iscentered  isfloating   monitor */
+  { "CherryTree",        NULL,       NULL,              1 << 3,       True,       True,       -1 },
+  { "Deadbeef",          NULL,       NULL,              1 << 4,       True,       True,       -1 },
+	{ "Deluge",            NULL,       NULL,              1 << 7,       True,       True,       -1 },
+  { "Firefox",           NULL,       NULL,              1 << 1,       False,      False,      -1 },
+  { "Termite",           NULL,       "weechat",         1 << 6,       False,      False,      -1 },
+  {  NULL,               NULL,       "glances",         1 << 4,       False,      False,      -1 },
+  { "Subl3",             NULL,       NULL,              1 << 2,       False,      False,      -1 },
+	{ "GIMP",              NULL,       NULL,              1 << 4,       False,      False,      -1 },
 };
 
 /* layout(s) */
@@ -101,8 +101,10 @@ static const char dmenu_lines[]    = "10";
 static const char *cmd_dmenu[] = { "launcher", "dmenu", "-s", dmenumon, "-fn", dmenufont, "-w", dmenu_width, "-h", dmenu_lheight, "-l", dmenu_lines, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, "-centerx", "-centery", NULL };
 static const char *cmd_term[]  = { "termite", NULL };
 static const char *cmd_browser[] = { "firefox", NULL, NULL, NULL, "Firefox" };
-static const char *cmd_chrome[] = { "chromium", NULL, NULL, NULL, "Firefox" };
+static const char *cmd_chrome[] = { "chromium", NULL, NULL, NULL, "Chromium" };
 static const char *cmd_sublime[] = { "subl3", NULL, NULL, NULL, "Subl3" };
+static const char *cmd_deluge[] = { "deluge-gtk", NULL, NULL, NULL, "Deluge" };
+static const char *cmd_cherry[] = { "cherrytree", NULL, NULL, NULL, "Deluge" };
 static const char *kb_backlightdec[] = { "xbacklight", "-dec", "10", NULL };
 static const char *kb_backlightinc[] = { "xbacklight", "-inc", "10", NULL };
 static const char *kb_launcher1[] = { "xbacklight", "-dec", "10", NULL };
@@ -211,6 +213,7 @@ static Command commands[] = {
    { "cmd_browser",     runorraise,     {.v = cmd_browser} },
    { "cmd_chrome",      runorraise,     {.v = cmd_chrome} },
    { "cmd_sublime",     runorraise,     {.v = cmd_sublime} },
+   { "cmd_deluge",      runorraise,     {.v = cmd_deluge} },
    { "togglebar",       togglebar,      {0} },
    { "focusstack+",     focusstack,     {.i = +1} },
    { "focusstack-",     focusstack,     {.i = -1} },
